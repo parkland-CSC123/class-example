7f21aceebd0630654b5c353b095aa2b867408332
Eric Barnard <eric.m.barnard@gmail.com>
2015-11-18 14:27:58 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello gitHub!" << endl;
+    cout << endl;
+    cout << "Student IDs from CSC 123, Fall 2015 follow:" << endl;
+    cout << "-------------------------------------------" << endl;
+
+    /// place your student ID here, it should look like:
+    /// cout << "000000000" << endl;
+
+
+
+
+    /***************************/
+    /*                         */
+    /*   no names please!      */
+    /*                         */
+    /***************************/
+
+    return 0;
+}
